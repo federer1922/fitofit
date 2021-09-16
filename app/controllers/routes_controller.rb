77c8d6b14dd394ea2fdf3a6c064ed21684efc94a
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-09-17 00:05:41 +0200

    create routes in index

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,2 +1,18 @@
 class RoutesController < ApplicationController
+  def create
+    route = Route.new
+    route.starting_adress = params["starting_adress"]
+    route.destination_adress = params["destination_adress"]
+    if route.save
+      redirect_to action: "index"
+    else
+      flash[:alert] = route.errors.full_messages.first
+
+      redirect_to action: "index"
+    end
+  end
+
+  def index
+    @routes = Route.all.order(:created_at)
+  end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,4 @@
 class Route < ApplicationRecord
+  validates :starting_adress, presence: true
+  validates :destination_adress, presence: true
 end
